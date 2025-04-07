b439ea1832a6fd0c6d81b9e409629f3f9218418f
addy <addyyy118@gmail.com>
2025-04-07 19:41:10 +0530

    refactor: remove unnecessary files

--- prisma/populate/user.js
+++
@@ -1,9 +0,0 @@
-const db = require("../../config/prismaClient");
-
-const main = async () => {
-  const res = await db.user.create({});
-
-  console.log(res);
-};
-
-main().catch((e) => console.error(e));
