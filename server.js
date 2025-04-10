3583688b9d24c4acb87ad38580de465019c8b1e1
addy <addyyy118@gmail.com>
2025-04-10 19:01:29 +0530

    chore: update cors for nested json

--- server.js
+++ server.js
@@ -15,8 +15,11 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.options("*", cors());
 
 const { PORT } = process.env;
 
