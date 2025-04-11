eebdd758287a20c1a7db9f7650e15a39a58b0f44
addy <addyyy118@gmail.com>
2025-04-11 16:29:06 +0530

    fix: display 0 rs for settle balances

--- src/components/GrpBalances.jsx
+++ src/components/GrpBalances.jsx
@@ -11,10 +11,14 @@ import UserPic from "./UserPic";
 import { Avatar } from "./ui/avatar";
 import { fetchBalances } from "@/utils/fetchGroupData";
 import { useParams } from "react-router-dom";
+import { useAuth } from "@/authProvider";
 
 export default function GrpBalances() {
   const { id: groupId } = useParams();
-  const { balances, setBalances } = useContext(GroupContext);
+  const { user } = useAuth();
+  const { group, balances, setBalances } = useContext(GroupContext);
+  const members = group?.members;
+  console.log(members);
 
   // refresh balance
   useEffect(() => {
@@ -34,35 +38,55 @@ export default function GrpBalances() {
 
       <CardContent>
         <ul className="space-y-4">
-          {balances.map((balance) => (
-            <li
-              key={balance.userId}
-              className="flex items-center justify-between"
-            >
-              <div className="flex items-center gap-2">
-                <Avatar className="h-8 w-8">
-                  <UserPic name={balance.name} />
-                </Avatar>
-                <span>{balance.name}</span>
-                {balance.isCurrentUser && (
-                  <span className="bg-muted rounded-full px-2 py-0.5 text-xs">
-                    You
-                  </span>
-                )}
-              </div>
-              {balance.balance > 0 ? (
-                <span className="font-medium text-green-600">
-                  +₹{balance.balance.toFixed(2)}
-                </span>
-              ) : balance.balance < 0 ? (
-                <span className="font-medium text-red-600">
-                  -₹{Math.abs(balance.balance).toFixed(2)}
-                </span>
-              ) : (
-                <span className="font-medium">₹0.00</span>
-              )}
-            </li>
-          ))}
+          {balances.length == 0
+            ? members?.map((member) => (
+                <li
+                  key={member.id}
+                  className="flex items-center justify-between"
+                >
+                  <div className="flex items-center gap-2">
+                    <Avatar className="h-8 w-8">
+                      <UserPic name={member.name} />
+                    </Avatar>
+                    <span>{member.name}</span>
+                    {member.id == user.id && (
+                      <span className="bg-muted rounded-full px-2 py-0.5 text-xs">
+                        You
+                      </span>
+                    )}
+                  </div>
+                  <span className="font-medium">₹0.00</span>
+                </li>
+              ))
+            : balances.map((balance) => (
+                <li
+                  key={balance.userId}
+                  className="flex items-center justify-between"
+                >
+                  <div className="flex items-center gap-2">
+                    <Avatar className="h-8 w-8">
+                      <UserPic name={balance.name} />
+                    </Avatar>
+                    <span>{balance.name}</span>
+                    {balance.isCurrentUser && (
+                      <span className="bg-muted rounded-full px-2 py-0.5 text-xs">
+                        You
+                      </span>
+                    )}
+                  </div>
+                  {balance.balance > 0 ? (
+                    <span className="font-medium text-green-600">
+                      +₹{balance.balance.toFixed(2)}
+                    </span>
+                  ) : balance.balance < 0 ? (
+                    <span className="font-medium text-red-600">
+                      -₹{Math.abs(balance.balance).toFixed(2)}
+                    </span>
+                  ) : (
+                    <span className="font-medium">₹0.00</span>
+                  )}
+                </li>
+              ))}
         </ul>
       </CardContent>
     </Card>
