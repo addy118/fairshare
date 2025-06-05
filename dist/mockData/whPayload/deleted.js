1ee0c1e193eefc7d9fccbab06f4553a5a0c6adb6
addy <addyyy118@gmail.com>
2025-06-05 17:18:35 +0530

    fix: update build command

--- dist/mockData/whPayload/deleted.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const webhookPayload = {
-    deleted: true,
-    id: "user_2xoGzvT1nGA0JkLQBcotjQazvW7",
-    object: "user",
-};
-//# sourceMappingURL=deleted.js.map
--- dist/routes/clerk.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const clerk_controller_1 = require("../controllers/clerk.controller");
-const clerkRouter = (0, express_1.Router)();
-clerkRouter.post("/user", (0, express_1.raw)({ type: "application/json" }), clerk_controller_1.postClerkUser);
-exports.default = clerkRouter;
-//# sourceMappingURL=clerk.routes.js.map
